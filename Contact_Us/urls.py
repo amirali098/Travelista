02f22731a2a2b404c213bb7873f15acb018c3e9d
Amirali Daghynaous <amirali_098@yahoo.com>
2024-04-26 02:26:57 +0330

    fix bugs

--- Contact_Us/urls.py
+++ Contact_Us/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('contact_us/', contact_us, name='contact_us'),
+    path('', contact_us, name='contact_us'),
 
 ]
--- Travelista/urls.py
+++ Travelista/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Blog.urls')),
-    path('/contact_us',include('Contact_Us.urls'))
+    path('contact_us',include('Contact_Us.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
