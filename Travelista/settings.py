a8330fc4ec738be3fc181b0c207bdf62b46a3376
Amirali Daghynaous <amirali_098@yahoo.com>
2024-04-26 02:22:09 +0330

    Put , after contact_us :))

--- Travelista/settings.py
+++ Travelista/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Blog',
-    'Contact_Us'
+    'Contact_Us',
     'django.contrib.humanize',
     'django_extensions',
     'django.contrib.sites',
